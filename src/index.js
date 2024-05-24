ae3fef81d00b9ea36c17d8a744da301d53447be7
Hardik <hardik2213105@akgec.ac.in>
2024-05-24 23:16:03 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express();
+
+;(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        app.on("error",()=>{
+            console.error("ERROR :",error);
+            throw err
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App listening at port ${process.env.PORT}`);
+        })
+    }catch(error){
+        console.error("ERROR :",error);
+        throw err
+    }
+})()
+*/
